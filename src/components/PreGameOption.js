0e1f5d837f9c4ac1dfacd58f301e4bbccc9ef84a
DizzlyD <david4henery00@hotmail.com>
2022-08-10 23:44:10 +0100

    menu dynamic

--- src/components/PreGameOption.js
+++ src/components/PreGameOption.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Lizard } from '../SVGs/svgs'
 
-const PreGameOption = (pos,colors,svg) => {
+const PreGameOption = (details) => {
+    // NEED the ".icon" for the svg component
+    const {pos,colors} = details
     return (
-        <div
+      <div
         className={`${pos} icon-wrap`}
         style={{
-        background: `linear-gradient(${colors.colorLight}, ${colors.colorDark})`,}}>
-            <div className="icon">{svg}</div>
-        </div>
+          background: `linear-gradient(${colors.colorLight}, ${colors.colorDark})`,
+        }}
+      >
+        <div className="icon"><details.icon/></div>
+      </div>
     );
 }
 
--- src/layouts/PreGame.js
+++ src/layouts/PreGame.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Pent, Lizard, Paper, Rock, Scissors, Spock } from "../SVGs/svgs";
 import PentSrc from "../images/bg-pentagon.svg"
 import playerOptions from "../optionsData/options";
-import PreGameOption from "../components";
+import { PreGameOption } from "../components";
 
 // Might have pent as background-image
 // An Image or svg
@@ -18,35 +18,12 @@ const PreGame = () => {
     <div className="pregame">
       {/* <Pent className="pregame__pent" /> */}
       <img className="pregame__pent" src={PentSrc} alt="" />
-      <div className="pregame__lizard icon-wrap icon-wrap-lizard">
-        <div className="icon">
-          <Lizard />
-        </div>
-      </div>
-      {/**/}
-      <div className="pregame__paper icon-wrap icon-wrap-paper">
-        <div className="icon">
-          <Paper />
-        </div>
-      </div>
-      {/**/}
-      <div className="pregame__rock icon-wrap icon-wrap-rock">
-        <div className="icon">
-          <Rock />
-        </div>
-      </div>
-      {/**/}
-      <div className="pregame__scissors icon-wrap icon-wrap-scissors">
-        <div className="icon">
-          <Scissors />
-        </div>
-      </div>
-      {/**/}
-      <div className="pregame__spock icon-wrap icon-wrap-spock">
-        <div className="icon">
-          <Spock />
-        </div>
-      </div>
+      {playerOptions.map(choice => {
+        const { preGameGridPos, colors, icon, id } = choice;
+        return (
+          <PreGameOption key={id} pos={preGameGridPos} colors={colors} icon={icon} />
+        );
+      })}
     </div>
   );
 }
